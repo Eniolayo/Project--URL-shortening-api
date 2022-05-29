d0ed4aedaf73d6c5d5b7bb2a74ba86f52396607c
Eniolayo <eniolayodeji@gmail.com>
2022-05-29 21:37:51 +0100

    changes

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
-const ham = document.querySelector('.header__icon-hambuger')
-const nav = document.querySelector('.header__nav') 
-const btnCopy = document.querySelector('.shortened__btn');
+const ham = document.querySelector(".header__icon-hambuger");
+const nav = document.querySelector(".header__nav");
+const btnShorten = document.querySelector(".cta__btn");
+const inputShorten = document.querySelector(".cta__input");
+const shortenedArea = document.querySelector(".shortened");
+const errorLink = document.querySelector(".cta__text i");
 
-nav.classList.add('.header__nav-show')
+nav.classList.add(".header__nav-show");
 
-ham.addEventListener('click', () => {
-    nav.classList.toggle('header__nav-show');
-})
+ham.addEventListener("click", () => {
+  nav.classList.toggle("header__nav-show");
+});
 
+btnShorten.addEventListener("click", () => {
+  shortenedURL().catch(() => {
+    errorLink.style.display = "block";
+    setTimeout(() => {
+      errorLink.style.display = "none";
+    }, 5000);
+  });
+});
+async function shortenedURL() {
+  const dataSent = await fetch(
+    `https://api.shrtco.de/v2/shorten?url=${inputShorten.value}`
+  );
+  const dataReceived = await dataSent.json();
+  const shortenedBox = document.createElement("div");
+  shortenedBox.innerHTML = ` <div class="shortened__box">
+                  <div class="shortened__input-value">
+                  <p>${inputShorten.value}</p>
+                  </div>
+                  <div class="shortened__inner-box">
+                   <div class="shortened__shorten-value">
+                  <p>${dataReceived.result.full_short_link2}</p>
+                  </div>
+                  <button class="shortened__btn">Copy</button>
+              </div>
+    </div>
+  `;
+  shortenedArea.prepend(shortenedBox);
+}
+const btnCopy = document.querySelector(".shortened__btn");
 
-
-btnCopy.addEventListener('click', () =>{
-    btnCopy.textContent = 'Copied!';
-})
+btnCopy.addEventListener("click", () => {
+  btnCopy.textContent = "Copied!";
+  btnCopy.style.backgroundColor = "rgb(34, 34, 218)";
+});
